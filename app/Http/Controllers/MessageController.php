9967e42b51d7d5ae17682ab13fdb4bb122d44919
milankaraja <naik.vineeth@gmail.com>
2022-10-01 21:00:16 +0200

    Initial commit

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Message;
+
+
+class MessageController extends Controller
+{
+
+    public function create(Request $request)
+         {
+        $message = new Message();
+        $message -> title = $request ->title;
+        $message -> content = $request ->content;
+
+        $message -> save();
+
+        return redirect('/about');
+        }
+    
+}
